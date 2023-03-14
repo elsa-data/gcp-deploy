54e1800d53220c20689821b9d5a886c44f924107
Christian Nassif-Haynes <ch.na.ha@gmail.com>
2023-03-14 12:52:17 +1100

    edgedb

--- edgedb.tf
+++ edgedb.tf
@@ -1,10 +1,4 @@
-resource "google_sql_database" "database" {
-  name     = "my-database"
-  instance = google_sql_database_instance.instance.name
-}
-
-# See versions at https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/sql_database_instance#database_version
-resource "google_sql_database_instance" "instance" {
+resource "google_sql_database_instance" "edgedb_postgres" {
   name             = "edgedb-postgres"
   region           = var.region
   database_version = "POSTGRES_13"
@@ -13,4 +7,26 @@ resource "google_sql_database_instance" "instance" {
   }
 
   deletion_protection  = "true"
+}
+
+resource "google_sql_user" "edgedb_postgres_users" {
+  name     = "postgres"
+  instance = google_sql_database_instance.edgedb_postgres.name
+  password = "changeme" // TODO: Document that this must be set
+}
+
+resource "google_container_cluster" "edgedb_k8s" {
+  name     = "edgedb-k8s"
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
+
+resource "google_container_node_pool" "edgedb_k8s" {
+  name       = "edgedb-k8s-node-pool"
+  cluster    = google_container_cluster.edgedb_k8s.id
+  node_count = 1
 }
